bc427eb3c87380240f2be0d5ac42c79a3f21bf40
tilyupo <tilyupo@gmail.com>
2025-11-19 12:32:01 +0300

    fix: revert trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,9 +18,6 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // Remove trailing slashes from URLs
-  trailingSlash: false,
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'replane-dev', // Usually your GitHub org/user name.
