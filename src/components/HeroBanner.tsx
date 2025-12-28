f8685e7cfc345fa806a750d2af56575fdae032eb
tilyupo <tilyupo@gmail.com>
2025-12-28 15:44:06 +0300

    update self-hosting link

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -55,7 +55,7 @@ export default function HeroBanner() {
             </Link>
 
             <Link
-              to='/docs/getting-started/quickstart'
+              to='/docs/getting-started/quickstart#self-hosted'
               className='inline-flex items-center gap-2 rounded-lg border border-stone-700 bg-stone-800 px-8 py-3.5 text-base font-semibold text-stone-100 transition-all duration-200 hover:border-stone-600 hover:bg-stone-700 hover:text-stone-100 hover:no-underline'
             >
               <Github className='h-4 w-4' />
