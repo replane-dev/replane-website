7411c84460c524d820a9bc4ea92575bcf52f88f2
tilyupo <tilyupo@gmail.com>
2026-01-09 01:21:11 +0300

    formatting

--- src/theme/BlogPostItems/index.tsx
+++ src/theme/BlogPostItems/index.tsx
@@ -39,7 +39,10 @@ interface BlogPostItemsProps {
   component?: React.ComponentType<{ children: React.ReactNode }>
 }
 
-export default function BlogPostItems({ items, component: BlogPostItemComponent = BlogPostItem }: BlogPostItemsProps) {
+export default function BlogPostItems({
+  items,
+  component: BlogPostItemComponent = BlogPostItem
+}: BlogPostItemsProps) {
   return (
     <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
       {items.map((blog) => (
